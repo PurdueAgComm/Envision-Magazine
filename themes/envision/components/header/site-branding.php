3f0aad0ff247376b9b62de23c3366d6458611bfd
Kenny Wilson <kenny@digital-inflection.com>
2017-04-19 15:19:37 -0400

    remove site branding

--- themes/envision/components/header/site-branding.php
+++ themes/envision/components/header/site-branding.php
@@ -1,4 +1,4 @@
-		<div class="site-branding">
+<!-- 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -12,4 +12,6 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
-		</div><!-- .site-branding -->
+		</div>-->
+
+
