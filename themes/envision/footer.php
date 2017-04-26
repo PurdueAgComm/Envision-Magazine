9cfa8468d1cccc29ec8d6088554b4d5d1b344de5
Kenny Wilson <kenny@digital-inflection.com>
2017-04-26 09:55:58 -0400

    create initial footer

--- themes/envision/footer.php
+++ themes/envision/footer.php
@@ -16,10 +16,24 @@
 
 <div class="section footer-span">
   <div class="container">
-	<footer id="colophon" class="site-footer" role="contentinfo">
-      <?php get_template_part( 'components/footer/site', 'info' ); ?>
-	</footer>
+  	<footer id="colophon" class="site-footer" role="contentinfo">
+        <?php get_template_part( 'components/footer/site', 'info' ); ?>
+  	</footer>
+  </div>
 </div>
+
+<div class="section ada-span">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p>Purdue Agriculture, 615 West State Street, West Lafayette, IN 47907-2053 USA, (765) 494-8392</p>
+        <p>&copy; <?php echo date("Y")?> Purdue University | An equal access/equal opportunity university | Integrity Statement | Copyright Complaints | <a href="https://ag.purdue.edu/agcomm">Maintained by Agricultural Communication</a></p>
+        <p>Trouble with this page? Disability-related accessibility issue? Please contact us at <a href="mailto:agweb@purdue.edu">agweb@purdue.edu</a> so we can help.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <?php wp_footer(); ?>
 
