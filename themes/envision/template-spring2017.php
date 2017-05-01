f4eecee20af24a2a02b92e386b7cadd4e3b30524
osmarlpz777 <olopez@purdue.edu>
2017-05-01 15:32:31 -0400

    Mobile Changes Starting
    Removed fog on small

--- themes/envision/template-spring2017.php
+++ themes/envision/template-spring2017.php
@@ -19,20 +19,24 @@ get_header('faded'); ?>
 
 #foglayer_01 {
   width: 200%;
+  min-height: 70vh;
   height: 100%;
   animation: foglayer_01_opacity 10s linear infinite, foglayer_moveme 15s linear infinite;
   -webkit-animation: foglayer_01_opacity 10s linear infinite, foglayer_moveme 15s linear infinite;
 }
 #foglayer_01 .image01, #foglayer_01 .image02 {
-  background: url("https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/05/fog1.png") repeat-x top left transparent;
+  background: url("/envision/wp-content/uploads/2017/05/fog1.png") repeat-x top left transparent;
   background-size: 100%;
   width: 50%;
+  min-height: 70vh;
   height: 100%;
   float: left;
+  background-repeat-y: repeat;
 }
 
 #foglayer_02 {
   width: 200%;
+  min-height: 70vh;
   height: 100%;
   position: absolute;
    top: 0;
@@ -42,15 +46,18 @@ get_header('faded'); ?>
         foglayer_moveme 13s linear infinite;
 }
 #foglayer_02 .image01, #foglayer_02 .image02 {
-  background: url("https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/05/fog2.png") repeat-x top left transparent;
+  background: url("/envision/wp-content/uploads/2017/05/fog2.png") repeat-x top left transparent;
   background-size: 100%;
   width: 50%;
+  min-height: 70vh;
   height: 100%;
   float: left;
+  background-repeat-y: repeat;
 }
 
 #foglayer_03 {
   width: 200%;
+  min-height: 70vh;
   height: 100%;
   position: absolute;
    top: 0;
@@ -60,11 +67,18 @@ get_header('faded'); ?>
         foglayer_moveme 13s linear infinite;
 }
 #foglayer_03 .image01, #foglayer_03 .image02 {
-  background: url("https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/05/fog2.png") repeat-x top left transparent;
+  background: url("/envision/wp-content/uploads/2017/05/fog2.png") repeat-x top left transparent;
   background-size: 100%;
   width: 50%;
   height: 100%;
   float: left;
+  background-repeat-y: repeat;
+}
+
+@media (max-width: 768px) {
+  .fog {
+    visibility: hidden;
+  }
 }
 
 @keyframes foglayer_01_opacity {
@@ -147,175 +161,182 @@ get_header('faded'); ?>
 
 </style>
 <!-- PRIMARY FEATURE -->
-<div class="landing" style="border-bottom: 7px solid #f87428;">
-  <div class="mag-headline">
+<div style="overflow-x: hidden;">
+  <div class="landing" style="border-bottom: 7px solid #f87428;">
+    <div class="mag-headline">
+      <div class="border"><h1 style="line-height: 1.4em;">FUNGI HUNTERS UNCOVER HIDDEN WORLDS</h1></div>
+      <div id="scroll-down" style="color: white;"><p>Read More &raquo;</p> </div>
+    </div>
+    <div id="foglayer_01" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+    <div id="foglayer_02" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+    <div id="foglayer_03" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+  </div>
+
+  <!-- TODO: change highlight color to that of story -->
+  <!-- <div class="landing" style="border-bottom: 7px solid #f87428;">
+    <div class="mag-headline">
     <div class="border"><h1 style="line-height: 1.4em;" >FUNGI HUNTERS UNCOVER HIDDEN WORLDS</h1></div>
-    <div id="scroll-down" style="color: white;"><p>Read More &raquo;</p> </div>
-  </div>
-  <div id="foglayer_01" class="fog">
-    <div class="image01"></div>
-    <div class="image02"></div>
-  </div>
-  <div id="foglayer_02" class="fog">
-    <div class="image01"></div>
-    <div class="image02"></div>
-  </div>
-  <div id="foglayer_03" class="fog">
-    <div class="image01"></div>
-    <div class="image02"></div>
-  </div>
-</div>
+      <div id="scroll-down" style="color: white;"><p>Read More &raquo;</p> </div>
+    </div>
+  </div> -->
+  <br>
 
-<!-- TODO: change highlight color to that of story -->
-<!-- <div class="landing" style="border-bottom: 7px solid #f87428;">
-  <div class="mag-headline">
-  <div class="border"><h1 style="line-height: 1.4em;" >FUNGI HUNTERS UNCOVER HIDDEN WORLDS</h1></div>
-    <div id="scroll-down" style="color: white;"><p>Read More &raquo;</p> </div>
-  </div>
-</div> -->
-<br>
-
-<!-- <?php
-  if ( have_posts() ) { ?>
-    <div id="content" class="site-content">
-      <div class="container">
-        <div class="row">
-          <div class="alert alert-info">
-          <p>
-            <?php
-              while ( have_posts() ) {
-                the_post();
-                  the_content();
-              } // end while
-              ?>
-          </p>
+  <!-- <?php
+    if ( have_posts() ) { ?>
+      <div id="content" class="site-content">
+        <div class="container">
+          <div class="row">
+            <div class="alert alert-info">
+            <p>
+              <?php
+                while ( have_posts() ) {
+                  the_post();
+                    the_content();
+                } // end while
+                ?>
+            </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <br>
-  <?php } // end if ?>
--->
+      <br>
+    <?php } // end if ?>
+  -->
 
-<!--TWO SECONDARY FEATURES -->
-<div class="section two-features">
-  <div class="container-fluid bleed-both">
-    <div class="col-sm-6 bleed-left">
-      <a href="#">
-        <!-- TODO: Change color to match story -->
-        <div class="col-sm-12 feature-two" style="border-bottom: 7px solid green;">
-          <div class="feature-headline-shader">
-            <h1 class="feature-headline">Launching Technology from Discovery to Delivery</h1>
+  <!--TWO SECONDARY FEATURES -->
+  <div class="section two-features">
+    <div class="container-fluid bleed-both">
+      <div class="col-sm-6 bleed-left">
+        <a href="#">
+          <!-- TODO: Change color to match story -->
+          <div class="col-sm-12 feature-two" style="border-bottom: 7px solid green;">
+            <img src="/envision/wp-content/uploads/2017/04/feature2.jpg" alt="Launching Technology from Discovery to Delivery" class="img-responsive">
+            <div class="feature-headline-shader">
+              <h1 class="feature-headline">Launching Technology from Discovery to Delivery</h1>
+            </div>
           </div>
-        </div>
-      </a>
-    </div>
-    <div class="col-sm-6 bleed-right">
-      <a href="#">
-        <!-- TODO: Change color to match story -->
-        <div class="col-sm-12 feature-three" style="border-bottom: 7px solid #5B6870;">
-          <div class="feature-headline-shader">
-            <h1 class="feature-headline">Taming the Bear Market</h1>
+        </a>
+      </div>
+      <div class="col-sm-6 bleed-right">
+        <a href="#">
+          <!-- TODO: Change color to match story -->
+          <div class="col-sm-12 feature-three" style="border-bottom: 7px solid #5B6870;">
+            <img src="/envision/wp-content/uploads/2017/04/bearmarket.jpg" alt="Taming Bear Market" class="img-responsive">
+            <div class="feature-headline-shader">
+              <h1 class="feature-headline">Taming the Bear Market</h1>
+            </div>
           </div>
-        </div>
-      </a>
-    </div>
-  </div>
-</div>
-
-<section class="hp-section popular-section stay">
-    <div class="col-sm-4">
-      <img src="https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/04/EverTrue.jpg" alt="Ever True - The Campaign for Purdue University" class="img-responsive evertrue-pic ">
-    </div>
-  <div class="items horizontal secondary">
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="story/you-can-book-luxury-hotels-for-minutes-at-a-time-with-this-new-app">Art</a></h5>
-        <h3 class="article-heading"><a href="story/you-can-book-luxury-hotels-for-minutes-at-a-time-with-this-new-app">Sculpture honoring pioneering entomologists</a></h3>
-      </div>
-    </div>
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="story/life-magazine-building-reborn-as-boutique-hotel-nyc">Tap Room</a></h5>
-        <h3 class="article-heading"><a href="story/life-magazine-building-reborn-as-boutique-hotel-nyc">Union Tap Room features local brews and science</a></h3>
-      </div>
-    </div>
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="galleries/2016-04-20/best-new-hotels-in-africa-and-middle-east-hot-list">Awards</a></h5>
-        <h3 class="article-heading"><a href="galleries/2016-04-20/best-new-hotels-in-africa-and-middle-east-hot-list">Best New Hotels in Africa and the Middle East: Hot List 2017</a></h3>
-      </div>
-    </div>
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Soundscapes</a></h5>
-        <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Vanishing Soundscapes Initiative featured on CNN</a></h3>
-      </div>
-    </div>
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Discovery</a></h5>
-        <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Breakthrough discovery for detecting cancer</a></h3>
-      </div>
-    </div>
-    <div class="item">
-      <div class="body">
-        <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Award</a></h5>
-        <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Plant scientist wins award for early-career innovation</a></h3>
+        </a>
       </div>
     </div>
   </div>
-</section>
 
-<!--Big Idea & Spotlights-->
-<div class="section big-idea-section parallax">
-  <div class="container">
+
+
+
+
+  <section class="hp-section popular-section stay">
       <div class="col-sm-4">
-        <img src="https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/05/big-idea-logo.png" alt="the big idea" class="img-responsive big-idea-pic">
+        <img src="/envision/wp-content/uploads/2017/04/EverTrue.jpg" alt="Ever True - The Campaign for Purdue University" class="img-responsive evertrue-pic ">
       </div>
-  </div>
-</div>
-
-<!--The View Ahead and Alumni section-->
-<div class="section viewahead-alumni">
-  <div class="container-fluid">
-    <div class="row">
-       <div class="col-md-5 col-md-offset-1 viewahead">
-        <div class="col-xs-6 deansignature">
-          <img src="https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/04/Jay-profile-signature-1.png" alt="Jay Adkrige" class="img-responsive dean">
+    <div class="items horizontal secondary">
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="story/you-can-book-luxury-hotels-for-minutes-at-a-time-with-this-new-app">Art</a></h5>
+          <h3 class="article-heading"><a href="story/you-can-book-luxury-hotels-for-minutes-at-a-time-with-this-new-app">Sculpture honoring pioneering entomologists</a></h3>
         </div>
-        <div class="col-xs-5 viewahead-text">
-          <h2 class="viewahead-title">The View Ahead</h2>
-          <p>Welcome to the inaugural edition of Envision, the new Purdue College of Agriculture magazine! Envision showcases the Purdue College of Agriculture’s distinctive strengths and global impact through the efforts of our faculty, staff, students, and alumni.</p>
-          <a href="/experience/Pages/index.html" class="btn btn-1 btn-1a pull-right">
-        Read More</a>
+      </div>
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="story/life-magazine-building-reborn-as-boutique-hotel-nyc">Tap Room</a></h5>
+          <h3 class="article-heading"><a href="story/life-magazine-building-reborn-as-boutique-hotel-nyc">Union Tap Room features local brews and science</a></h3>
         </div>
-     </div>
-      <div class="col-md-5 alumni-column">
-        <img src="https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/04/alumnicloseup.jpg" alt="Alumni Close Up" class="img-responsive alumni-pic">
-        <div class="alumni-headline-shader">
-          <h2 class="alumni-headline">Alumni Close Up</h2>
+      </div>
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="galleries/2016-04-20/best-new-hotels-in-africa-and-middle-east-hot-list">Awards</a></h5>
+          <h3 class="article-heading"><a href="galleries/2016-04-20/best-new-hotels-in-africa-and-middle-east-hot-list">Best New Hotels in Africa and the Middle East: Hot List 2017</a></h3>
+        </div>
+      </div>
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Soundscapes</a></h5>
+          <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Vanishing Soundscapes Initiative featured on CNN</a></h3>
+        </div>
+      </div>
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Discovery</a></h5>
+          <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Breakthrough discovery for detecting cancer</a></h3>
+        </div>
+      </div>
+      <div class="item">
+        <div class="body">
+          <h5 class="channel"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Award</a></h5>
+          <h3 class="article-heading"><a href="story/domaine-des-etangs-memorable-stay-french-chateau">Plant scientist wins award for early-career innovation</a></h3>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  </section>
 
-<!--Ever True Campaign (Not a main feature area)
-<div class="section evertrue-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-6">
-      <img src="https://dev.ag.purdue.edu/envision/wp-content/uploads/2017/04/EverTrue.jpg" alt="Ever True - The Campaign for Purdue University" class="img-responsive">
-      </div>
-       <div class="col-sm-6">
-          <h2 class="evertrue-header">Ever True Campaign</h2>
-          <p>The Ever True campaign to advance Purdue as a national and global leader spans July 2012 through June 2019. See what generous support of the College of Agriculture has accomplisarticle-heading so far. </p>
+  <!--Big Idea & Spotlights-->
+  <div class="section big-idea-section parallax">
+    <div class="container">
+        <div class="col-sm-4">
+          <img src="/envision/wp-content/uploads/2017/05/The-Big-Idea-ver1.png" alt="the big idea" class="img-responsive big-idea-pic">
+        </div>
+    </div>
+  </div>
+
+  <!--The View Ahead and Alumni section-->
+  <div class="section viewahead-alumni">
+    <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-5 col-md-offset-1 viewahead">
+          <div class="col-xs-6 deansignature">
+            <img src="/envision/wp-content/uploads/2017/04/Jay-profile-signature-1.png" alt="Jay Adkrige" class="img-responsive dean">
+          </div>
+          <div class="col-xs-5 viewahead-text">
+            <h2 class="viewahead-title">The View Ahead</h2>
+            <p>Welcome to the inaugural edition of Envision, the new Purdue College of Agriculture magazine! Envision showcases the Purdue College of Agriculture’s distinctive strengths and global impact through the efforts of our faculty, staff, students, and alumni.</p>
+            <a href="/experience/Pages/index.html" class="btn btn-1 btn-1a pull-right">
+          Read More</a>
+          </div>
        </div>
+        <div class="col-md-5 alumni-column">
+          <img src="/envision/wp-content/uploads/2017/04/alumnicloseup.jpg" alt="Alumni Close Up" class="img-responsive alumni-pic">
+          <div class="alumni-headline-shader">
+            <h2 class="alumni-headline">Alumni Close Up</h2>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div> -->
 
+  <!--Ever True Campaign (Not a main feature area)
+  <div class="section evertrue-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+        <img src="/envision/wp-content/uploads/2017/04/EverTrue.jpg" alt="Ever True - The Campaign for Purdue University" class="img-responsive">
+        </div>
+         <div class="col-sm-6">
+            <h2 class="evertrue-header">Ever True Campaign</h2>
+            <p>The Ever True campaign to advance Purdue as a national and global leader spans July 2012 through June 2019. See what generous support of the College of Agriculture has accomplisarticle-heading so far. </p>
+         </div>
+      </div>
+    </div>
+  </div> -->
+</div>
 
 
 
