b2b74c88e0b6e7e1393990e2b1c39b77c5dfebae
Kenny Wilson <kenny@digital-inflection.com>
2017-05-02 15:30:48 -0400

    bootstrapped

--- themes/envision/page.php
+++ themes/envision/page.php
@@ -13,12 +13,13 @@
  */
 
 get_header(); ?>
+
 	<div id="content" class="site-content">
     <div class="container">
       <div class="row">
-      	<div class="col-sm-8">
-					<div id="primary" class="content-area">
-						<main id="main" class="site-main" role="main">
+      	<div class="col-sm-12">
+					<div id="primary">
+						<main id="main" role="main">
 
 							<?php
 							while ( have_posts() ) : the_post();
@@ -34,8 +35,7 @@ get_header(); ?>
 							?>
 						</main>
 					</div>
-				</div> <!-- /.col-sm-8-->
+				</div> <!-- /.col-sm-12-->
 
 <?php
-get_sidebar();
 get_footer();
