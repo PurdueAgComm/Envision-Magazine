f2f5b5878f7cb0b813d7d5bc06b546d7b4a5a763
Kenny Wilson <kenny@digital-inflection.com>
2017-05-02 13:30:32 -0400

    initial post stylings

--- themes/envision/single.php
+++ themes/envision/single.php
@@ -6,30 +6,48 @@
  *
  * @package Envision_Magazine
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+  $postID = get_the_ID();
+$featuredURL = get_the_post_thumbnail_url($postID, "full");
+// if post has featured image, display it in a banner
+if(!empty($featuredURL)) : ?>
+	<div class="landing" style="background-image: url('<?php echo get_the_post_thumbnail_url($postID, "full"); ?>')"></div>
+<?php endif; ?>
 
-		<?php
-		while ( have_posts() ) : the_post();
+<div class="container story-body">
+  <div class="row">
+    <div class="maincontent blog-maincontent col-md-offset-1 col-md-10">
+        <h1 class="blog-title"><?php the_title(); ?></h1>
+        <div class="blog-date text-muted"><i class="fa fa-calendar"></i> <?php the_time('l, F jS, Y'); ?></div>
 
-			get_template_part( 'components/post/content', get_post_format() );
+        <?php the_content(); ?>
 
-			the_post_navigation();
+        <?php if(has_category() || has_tag()) : ?>
+          <div class="blog-meta">
+            <?php if(has_category()) : ?>
+              <i class="fa fa-folder" title="category"></i> Category <?php the_category(', '); ?> <br>
+            <?php endif; ?>
+            <?php if(has_tag()) : ?>
+              <i class="fa fa-tags"></i> Tag: <?php the_tags(''); ?>
+            <?php endif; ?>
+          </div>
+        <?php endif; ?>
+        <br>
+        <nav id="nav-single">
+          <h3 class="sr-only"><?php _e( 'Read more', 'purduetwentyfourteen' ); ?></h3>
+          <div class="pull-left"><?php previous_post_link( '%link', __( '<i class="fa fa-arrow-circle-left"></i> %title', 'purduetwentyfourteen' ) ); ?></div>
+          <div class="pull-right"><?php next_post_link( '%link', __( '%title <i class="fa fa-arrow-circle-right"></i>', 'purduetwentyfourteen' ) ); ?></div>
+        </nav>
+        <br><br>
+    </div>
+  </div>
+</div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+<?php endwhile; else: ?>
+  <p><?php _e('Sorry, this page does not exist.'); ?></p>
+<?php endif; ?>
 
-		endwhile; // End of the loop.
-		?>
 
-		</main>
-	</div>
-<?php
-get_sidebar();
-get_sidebar( '2' );
-get_footer();
+<?php get_footer(); ?>
