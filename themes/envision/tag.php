ac31d6c39ec7cdfd8af3dd7643c4220ba733bafe
Kenny Wilson <kenny@digital-inflection.com>
2017-05-03 19:50:09 -0400

    archive, category, and tag page designs for archiving

--- themes/envision/tag.php
+++ themes/envision/tag.php
@@ -1,14 +1,5 @@
+<?php get_header(); ?>
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Envision_Magazine
- */
-
-get_header();
-
   // remove the [...] from the_excerpt output and just adds an elipses
   function new_excerpt_more( $more ) {
       return '...';
@@ -19,10 +10,10 @@ get_header();
 <div class="container">
   <div class="row">
     <div class="maincontent col-md-8">
-      <?php the_archive_title( '<h1 class="page-title">', ' Stories</h1>' ); ?>
+    <h1><?php ucfirst(single_tag_title()); ?> Stories</h1>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <div class="row post-bg" style="border-left: 7px solid #ffd100; margin-bottom: 30px;">
+      <div class="row post-bg" style="border-left: 7px solid #ffd100;">
         <div class="col-sm-12">
           <?php the_post_thumbnail('small'); ?>
           <h2 style="padding-top: 0"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
