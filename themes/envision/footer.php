b6f0a39222b92656474efd7f0c0af71255f6ff67
Kenny Wilson <kenny@digital-inflection.com>
2017-05-04 10:25:49 -0400

    add sharing

--- themes/envision/footer.php
+++ themes/envision/footer.php
@@ -36,6 +36,7 @@
 
 
 <?php wp_footer(); ?>
-
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=agcomm"></script>
 </body>
 </html>
